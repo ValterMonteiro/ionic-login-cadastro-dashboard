42f761c769f24e774164c7c116597b1b5f5bfc68
ValterMonteiro <valtinms@gmail.com>
2023-08-15 18:28:40 -0400

    criando pagina dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'cadastro',
     loadChildren: () => import('./pagina/cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pagina/dashboard/dashboard.module').then( m => m.DashboardPageModule)
   }
 ];
 
