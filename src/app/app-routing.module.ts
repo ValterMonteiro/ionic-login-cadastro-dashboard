f08dfd08100179df2166e9b9de3be3ba7692ae89
ValterMonteiro <valtinms@gmail.com>
2023-08-15 18:28:09 -0400

    criando pagina cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pagina/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./pagina/cadastro/cadastro.module').then( m => m.CadastroPageModule)
   }
 ];
 
